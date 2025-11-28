1976a517389b9ba78ae11046b7f67936022c2199
BrayamAristaF <brayamaristafrndz@gmail.com>
2025-11-28 00:48:29 -0500

    Cuerpo 0.1

---
+++ web/src/main/java/web/lol/web/WeblolApplication.java
@@ -0,0 +1,13 @@
+package web.lol.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblolApplication.class, args);
+	}
+
+}
